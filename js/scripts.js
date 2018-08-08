d1acf2b913d803c88302921082b92cc8613785be
zhuleqiang and Hyung Lee <zhuleqiang@>
2018-08-08 13:22:08 -0700

    Add content to modal.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,14 @@ $(document).ready(function() {
 
   $("#myBtn").click(function() {
     $(".modal").show();
-  })
+  });
   $(".close").click(function() {
     $(".modal").hide();
-  })
+  });
+
+  $("form#inputBox").submit(function(event) {
+    event.preventDefault();
+    $(".modal").hide();
+    $(".confirmation").show();
+  });
 })
